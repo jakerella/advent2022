ffb192769099cbf4d2282201189370d51f4e8cd2
Jordan Kasper <jordan@jordankasper.com>
2022-12-09 09:16:35 -0500

    day 8, p2

--- src/advent/day8.rs
+++ src/advent/day8.rs
@@ -25,7 +25,21 @@ pub fn day8(input: String) {
         }
     }
 
-    println!("Part 1answer {:?}", unblocked);
+    println!("Part 1 answer (unblocked): {:?}", unblocked);
+
+    let mut highest_score: u32 = 0;
+    for y in (0..forest.len()) {
+        for x in (0..forest[y].len()) {
+            let score = get_score(&forest, y, x);
+            // println!("score for {:?}, {:?} is {:?}", y, x, score);
+            if score > highest_score {
+                highest_score = score;
+            }
+        }
+    }
+
+    // 136: low
+    println!("Part 2 answer (highest score): {:?}", highest_score);
 }
 
 fn is_visible(forest: &Vec<Vec<u8>>, y:usize, x:usize) -> bool {
@@ -45,4 +59,52 @@ fn is_visible(forest: &Vec<Vec<u8>>, y:usize, x:usize) -> bool {
     for down in (y+1..forest.len()) { if forest[down][x] >= h { down_vis = false; } }
 
     return left_vis || right_vis || up_vis || down_vis;
+}
+
+fn get_score(forest: &Vec<Vec<u8>>, y:usize, x:usize) -> u32 {
+    let h = forest[y][x];
+    let mut left_score: u32 = 0;
+    let mut right_score: u32 = 0;
+    let mut up_score: u32 = 0;
+    let mut down_score: u32 = 0;
+
+    if x > 0 {
+        for left in (0..x) {
+            left_score += 1;
+            if forest[y][x-(left+1)] >= h {
+                // println!("that's it, {:?} is >= {:?}", forest[y][x-(left+1)], h);
+                break;
+            }
+        }
+    }
+    // println!("left score for {:?}, {:?} is {:?}", y, x, left_score);
+    if x < (forest[y].len()-1) {
+        for right in (x+1..forest[y].len()) {
+            right_score += 1;
+            if forest[y][right] >= h {
+                break;
+            }
+        }
+    }
+    // println!("right score for {:?}, {:?} is {:?}", y, x, right_score);
+    if y > 0 {
+        for up in (0..y) {
+            up_score += 1;
+            if forest[y-(up+1)][x] >= h {
+                break;
+            }
+        }
+    }
+    // println!("up score for {:?}, {:?} is {:?}", y, x, up_score);
+    if y < (forest.len()-1) {
+        for down in (y+1..forest.len()) {
+            down_score += 1;
+            if forest[down][x] >= h {
+                break;
+            }
+        }
+    }
+    // println!("down score for {:?}, {:?} is {:?}", y, x, down_score);
+
+    return left_score * right_score * up_score * down_score;
 }
